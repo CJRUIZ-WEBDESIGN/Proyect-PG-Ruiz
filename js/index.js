d8ea556e0b53380095e820c63590af1ae7390585
Cristian Javier Ruiz <cjruiz2013@gmail.com>
2022-06-24 11:06:26 -0300

    Version Final - Comienzo JavaScript - Creacion de Carpeta y index.jss + Vinculacion de index.js necesario para la Cursada

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+// En cada repetición, calculamos el número ingresado x el número de repetición (i)
+for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero * i ;
+    alert(ingresarNumero +" X "+ i +" = "+ resultado);
+}
+

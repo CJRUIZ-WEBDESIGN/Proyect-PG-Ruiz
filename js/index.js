c3926a815b9a2fee932f82be47ee5df4378c98d4
LuZBeLiTo0 <cjruiz2013@gmail.com>
2022-06-28 20:16:45 -0300

    ProyectoFinal - Vinculacion de Js y creacion de index.js en carpeta

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
-let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+/* let ingresarNumero = parseInt(prompt("Ingresar Numero"));
 // En cada repetición, calculamos el número ingresado x el número de repetición (i)
 for (let i = 1; i <= 10; i++) {
     let resultado = ingresarNumero * i ;
     alert(ingresarNumero +" X "+ i +" = "+ resultado);
+} */
+/* 
+function saludo(nombre, apellido){
+    
+    
+    console.log(`hola como estas ${nombre} ${apellido}?`);
 }
+ saludo("camila", "ruiz"); */
 
+/*  function sumar (num1, num2){
+
+    return num1 + num2;
+ }
+}let resultado = sumar (2, 4);
+console.log(resultado + 20); */
+
+
+/* function casos(){
+
+    if(caso == "hola"){
+        return "Hola"
+    }
+
+    if(caso === ("asdasd")){
+        return "Chau"
+    }
+}   
+
+casos("hola", "chau") */

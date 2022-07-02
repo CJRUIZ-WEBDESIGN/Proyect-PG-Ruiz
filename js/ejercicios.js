39086c87fcf3030c3e395105e3d4ebfa40ace733
LuZBeLiTo0 <cjruiz2013@gmail.com>
2022-07-02 20:58:05 -0300

    ProGamerArg - Version de Pruebas de JS

---
+++ js/ejercicios.js
@@ -0,0 +1,137 @@
+//Ejercicios N° 1: Homero 
+
+/* let nombre = "Homero";
+let apellido = "Simpsons";
+let edad = 39; 
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad); */
+
+
+//Ejercicios N° 2: Ciudades
+
+/* const ciudad1 = "Springfield";
+const ciudad2 = "Shelbyville";
+const ciudad3 = "Capital City";
+const ciudad4 = "Cypress Creek";
+const ciudad5 = "Ogdenville";
+
+console.log(ciudad1);
+console.log(ciudad2);
+console.log(ciudad3);
+console.log(ciudad4);
+console.log(ciudad5); */
+
+/* let persona         = "BART SIMPSONS";
+let domicilio       = "742 EVERGREEN";
+let pais            = "USA";
+let nacimiento      = "02-11-70";
+const codigo        = "B47U89RE243";
+let carnet = "Codigo: "+codigo+"  "+
+             "Nombre: "+persona+"  "+
+             "Pais: "+pais+"  "+
+             "Nacimiento: "+nacimiento+"  "+
+             "Domicilio: "+domicilio;
+console.log(carnet); */
+
+
+/* let integrante1 = prompt("INGRESAR 1ER INTEGRANTE");
+let integrante2 = prompt("INGRESAR 2DO INTEGRANTE");
+let integrante3 = prompt("INGRESAR 3RO INTEGRANTE");
+let integrante4 = prompt("INGRESAR 4TO INTEGRANTE");
+let integrante5 = prompt("INGRESAR 5TO INTEGRANTE");
+
+let familia =   "1) "+integrante1+" "+
+                "2) "+integrante2+" "+
+                "3) "+integrante3+" "+
+                "4) "+integrante4+" "+
+                "5) "+integrante5;
+
+
+console.log(familia); */
+
+//DESCUENTO, NO FUNCIONA? 
+
+/* let precio = parseFloat(prompt("INGRESA PRECIO")); 
+let descuento20 = precio - (precio * 0.2);
+let descuento30 = precio - (precio * 0.3);
+
+console.log(descuento20);
+console.log(descuento30); */
+
+// BART YO NO FUI - FUI YO
+
+/* let entrada = prompt("INGRESAR UN NOMBRE");
+let nombre = "BART"; 
+if (entrada == nombre) {
+     alert("FUI YO"); 
+} else {
+    alert ("YO NO FUI");
+} */
+
+// PRESIONAR Y 
+
+/* let entrada2 = prompt("INGRESAR UNA TECLA YES(Y) OR NOT(N)");
+if ((entrada2 == "y") || (entrada2 == "Y")) {
+    alert("CORRECTO");
+} else {
+    alert("ERROR");
+} */
+
+//TE SALUDO O NO TE SALUDO... CORTA
+/*  let salida = prompt("Ingresa el Nombre");
+let saludo = "Santiago"; 
+if (salida == saludo) {
+     alert(`Hola ${salida}`); 
+} else {
+    alert ("No te saludo");
+}  */
+
+//ALGO RELACIONADO CON PROGAMER PRODUCTOS ARMA TU PC
+/* 
+alert("ARMA TU PC: ELEGI TU MICRO")
+let entrada3 = prompt("AMD O INTEL"); 
+let entrada2 = AMD;
+let entrada1 = INTEL;
+
+if (entrada3 == ) 
+{
+alert ("ELEGISTE A HOMERO")
+} */
+
+
+//ELEGIR UN PERSONAJE DE LOS SIMPSONS 
+
+
+/* let entrada3 = prompt("INGRESAR UN NUMERO");
+if (entrada3 === 1)       {
+    alert("ELEGISTE A HOMERO");
+} else if (entrada3 == 2) {
+    alert("ELEGITE A MARGE");
+}else if (entrada3 == 3)  {
+    alert("ELEGISTE A BART");
+}else if (entrada3 == 4)  {
+    alert("ELEGISTE A LISA");
+}
+else {
+    alert("ERROR");
+} */
+
+//TIPO DE PRESUPUESTO. 
+/* let entrada4 = parseFloat(prompt("INGRESAR UN NUMERO"));
+if ((entrada4 >= 0) && (entrada4 <= 1000)){
+    alert("PRESUPUESTO BAJO");
+
+}else if ((entrada4 >= 1001) && (entrada4 <= 3000)){
+    alert("PRESUPUESTO MEDIO");
+
+}else if ((entrada4 >= 3000)) {
+    alert("PRESUPUESTO ALTO");
+
+}
+else {
+
+    alert("ERROR");
+} */
+
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+/* 
 //APLICACION DE FOR PARA PROMPT "CAPITAL ARGENTINA". 
 
 let respuesta = "BUENOS AIRES"; 	 	 
@@ -11,4 +11,76 @@ for (let contador = 0; contador < 3 && acierto === false; contador++) {
 	    }	 	 
 }	 	 
 
-if (!acierto) {alert("Lo siento, agotaste tus 3 intentos. Vuelva a intentarlo.")}; 
+if (!acierto) {alert("Lo siento, agotaste tus 3 intentos. Vuelva a intentarlo.")};  */
+
+/* let nombre = "Pepito";
+let edad = 34; 
+let direccion = "AV";
+
+const persona1 = {nombre: "Pepito", edad: 34, direccion: "AV"};  */
+
+/* function Persona(nombre, edad, direccion){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.direccion = direccion; 
+    
+}
+
+const persona1 = new Persona("Cristian", 38, "AV");
+
+console.log(persona1); */
+
+/* class Producto {
+    constructor(nombre, precio){
+        this.nombre = nombre; 
+        this.precio = precio;
+        this.vendido = false; 
+    }
+    vender(){
+        this.vendido = true;
+    }       
+}
+
+const producto1 = new Producto("Arroz", 1000);
+const producto2 = new Producto("Fideo", 1500);
+
+console.log(producto1);
+producto1.vender();
+console.log(producto1);
+
+ */
+
+/* class ProductoMercado{
+    constructor(nombre, precio, img){
+        this.nombre = nombre; 
+        this.precio = precio;
+        this.img = img; 
+        this.vendido = false; 
+    }
+    vender(){
+        this.vendido = true;
+    }
+}
+
+const producto1 = new ProductoMercado("Monitor LED para Juegos...", 1494900, "ImagenProducto");
+console.log(producto1); */
+
+
+//
+
+
+/* class ProductoMercado{
+    constructor(nombre, precio, img){
+        this.sku = sku; 
+        this.producto = producto;
+        this.img = img; 
+        this.vendido = false; 
+    }
+    vender(){
+        this.vendido = true;
+    }
+}
+
+const producto1 = new ProductoMercado("Monitor LED para Juegos...", 1494900, "ImagenProducto");
+console.log(producto1);*/
+
